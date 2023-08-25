09a6194fe12e0a07de773c5587849dfc45b57849
MohamedHamed12 <mh1779371@gmail.com>
2023-08-25 22:30:51 +0300

    edit_for_host

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 # ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 # ALLOWED_HOSTS = ['*','.vercel.app', '.now.sh']
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*','.vercel.app', '.now.sh']
 # Application definition
 
 
