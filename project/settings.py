d2cd92e56b20e0889aa4d8eaa1d8f29e0cdc0037
MohamedHamed12 <mh1779371@gmail.com>
2023-08-25 22:06:12 +0300

    edit_for_host

--- project/settings.py
+++ project/settings.py
@@ -93,13 +93,22 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'postgres',
+        'USER': 'postgres',
+        'PASSWORD':'@##moamed##',
+        'HOST': 'db.wlrrgqugsepxkdrjblpu.supabase.co',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
